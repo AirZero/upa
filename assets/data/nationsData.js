0ca6422e89f471444c16e90f92bcb4ea76f1dacf
AirZero <airzero@gmail.com>
2015-03-18 15:03:41 +0200

    ita-eurooppa lisatty

--- assets/data/nationsData.js
+++ assets/data/nationsData.js
@@ -14,287 +14,107 @@ function getJsonFromNationsData(){
 			"name": "Venaja",
 			"x": " ,Border:Right",
 			"y": 600,
-			"width": 187,
-			"height": 348,
+			"width": 637,
+			"height": 616,
 			"rotation": 0,
 			"sprite": "Venaja"
 		
 		},		
 		{
 			"name": "Norja",
-			"x": "Left:Venaja,Percent:19",
-			"y": "Same:Venaja,Percent:-45",
-			"width": 258,
-			"height": 207,
+			"x": "Left:Venaja,Percent:42",
+			"y": "Same:Venaja,Percent:-76",
+			"width": 763,
+			"height": 248,
 			"rotation": 0,
 			"sprite": "Norja"
 		
 		},
 		{
 			"name": "Suomi",
-			"x": "Left:Venaja,Percent:38",
-			"y": "Same:Venaja,Percent:-60",
-			"width": 120,
-			"height": 159,
+			"x": "Left:Venaja,Percent:100",
+			"y": "Same:Venaja,Percent:-150.5",
+			"width": 353,
+			"height": 154,
 			"rotation": 0,
 			"sprite": "Suomi"
 		},
-		{
+			{
 			"name": "Ruotsi",
-			"x": "Same:Suomi,Percent:-57.5",
-			"y": "Same:Suomi,Percent:19",
-			"width": 129,
-			"height": 191,
+			"x": "Same:Suomi,Percent:-56.5",
+			"y": "Same:Suomi,Percent:27",
+			"width": 424,
+			"height": 287,
 			"rotation": 0,
 			"sprite": "Ruotsi"
 		
 		},
 		{
 			"name": "Viro",
-			"x": "Left:Venaja,Percent:17",
-			"y": "Down:Suomi,Percent:-17",
-			"width": 66,
-			"height": 34,
+			"x": "Left:Venaja,Percent:136",
+			"y": "Down:Suomi,Percent:1",
+			"width": 201,
+			"height": 59,
 			"rotation": 0,
 			"sprite": "Viro"	
 		},
 		{
 			"name": "Latvia",
-			"x": "Same:Viro,Percent:-8",
-			"y": "Down:Viro,Percent:-37",
-			"width": 69,
-			"height": 28,
+			"x": "Same:Viro,Percent:-6",
+			"y": "Down:Viro,Percent:-31",
+			"width": 244,
+			"height": 61,
 			"rotation": 0,
 			"sprite": "Latvia"	
 		},
+		
 		{
 			"name": "Valko-Venaja",
-			"x": "Same:Latvia,Percent:36",
-			"y": "Down:Latvia,Percent:-13",
-			"width": 91,
-			"height": 50,
+			"x": "Same:Latvia,Percent:38",
+			"y": "Down:Latvia,Percent:-11.5",
+			"width": 306,
+			"height": 125,
 			"rotation": 0,
 			"sprite": "Valko-Venaja"	
-		},
+		}	,
 		{
 			"name": "Liettua",
-			"x": "Same:Latvia,Percent:-11",
-			"y": "Down:Latvia,Percent:-34",
-			"width": 55,
-			"height": 29,
+			"x": "Same:Latvia,Percent:-11.3",
+			"y": "Down:Latvia,Percent:-28",
+			"width": 193,
+			"height": 70,
 			"rotation": 0,
 			"sprite": "Liettua"	
 		},
-		{
+			{
 			"name": "Ukraina",
-			"x": "Same:Valko-Venaja,Percent:15.5",
-			"y": "Down:Valko-Venaja,Percent:-13",
-			"width": 176,
-			"height": 75,
+			"x": "Same:Valko-Venaja,Percent:10.1",
+			"y": "Down:Valko-Venaja,Percent:-12.4",
+			"width": 531,
+			"height": 202,
 			"rotation": 0,
 			"sprite": "Ukraina"	
-		},
-		{
-			"name": "Puola",
-			"x": "Left:Valko-Venaja,Percent:7",
-			"y": "Same:Valko-Venaja,Percent:33",
-			"width": 92,
-			"height": 58,
-			"rotation": 0,
-			"sprite": "Puola"	
-		},
-		{
-			"name": "Saksa",
-			"x": "Left:Puola,Percent:9",
-			"y": "Same:Puola,Percent:11",
-			"width": 84,
-			"height": 72,
-			"rotation": 0,
-			"sprite": "Saksa"	
-		},
-		{
-			"name": "Tanska",
-			"x": "Same:Saksa,Percent:15",
-			"y": "Up:Saksa,Percent:16",
-			"width": 71,
-			"height": 39,
-			"rotation": 0,
-			"sprite": "Tanska"	
-		},
-		{
-			"name": "Benelux",
-			"x": "Left:Saksa,Percent:43",
-			"y": "Same:Saksa,Percent:-2",
-			"width": 40,
-			"height": 38,
-			"rotation": 0,
-			"sprite": "Benelux"	
-		},
-		{
-			"name": "Ranska",
-			"x": "Left:Saksa,Percent:27",
-			"y": "Down:Saksa,Percent:-40",
-			"width": 120,
-			"height": 82,
-			"rotation": 0,
-			"sprite": "Ranska"	
-		},
-		{
-			"name": "Italia",
-			"x": "Same:Saksa,Percent:15",
-			"y": "Down:Saksa,Percent:0",
-			"width": 102,
-			"height": 86,
-			"rotation": 0,
-			"sprite": "Italia"	
-		},
-		{
-			"name": "Romania",
-			"y": "Down:Ukraina,Percent:-88",
-			"x": "Same:Ukraina,Percent:-69",
-			"width": 89,
-			"height": 40,
-			"rotation": 0,
-			"sprite": "Romania"	
-		},
-		{
-			"name": "Moldova",
-			"x": "Same:Ukraina,Percent:-89",
-			"y": "Down:Ukraina,Percent:-135",
-			"width": 33,
-			"height": 29,
-			"rotation": 0,
-			"sprite": "Moldova"	
-		},
-		{
-			"name": "Sveitsi",
-			"x": "Same:Italia,Percent:-80.5",
-			"y": "Same:Italia,Percent:-215",
-			"width": 44,
-			"height": 18,
-			"rotation": 0,
-			"sprite": "Sveitsi"	
-		},
-		{
-			"name": "Espanja",
-			"x": "Same:Ranska,Percent:-45",
-			"y": "Down:Ranska,Percent:-31",
-			"width": 102,
-			"height": 59,
-			"rotation": 0,
-			"sprite": "Espanja"	
-		},
-				{
-			"name": "Portugal",
-			"x": "Left:Espanja,Percent:100",
-			"y": "Same:Espanja,Percent:8",
-			"width": 27,
-			"height": 39,
-			"rotation": 0,
-			"sprite": "Portugal"	
-		},
-		{
-			"name": "Itavalta",
-			"x": "Right:Sveitsi,Percent:-18",
-			"y": "Same:Sveitsi,Percent:-30",
-			"width": 70,
-			"height": 24,
-			"rotation": 0,
-			"sprite": "Itavalta"	
-		},
-				{
-			"name": "Tsekki",
-			"x": "Right:Saksa,Percent:-40",
-			"y": "Same:Saksa,Percent:61",
-			"width": 73,
-			"height": 23,
-			"rotation": 0,
-			"sprite": "Tsekki"	
-		},
-			{
-			"name": "Slovakia",
-			"x": "Right:Tsekki,Percent:-55",
-			"y": "Same:Tsekki,Percent:72",
-			"width": 52,
-			"height": 16,
-			"rotation": 0,
-			"sprite": "Slovakia"	
-		},
-		
-		{
-			"name": "Unkari",
-			"x": "Right:Itavalta,Percent:-13",
-			"y": "Down:Slovakia,Percent:-32",
-			"width": 60,
-			"height": 26,
-			"rotation": 0,
-			"sprite": "Unkari"	
-		},
-		
-		{
-			"name": "Entinen-Jugoslavia",
-			"x": "Right:Italia,Percent:-48",
-			"y": "Same:Italia,Percent:-25",
-			"width": 86,
-			"height": 52,
-			"rotation": 0,
-			"sprite": "Entinen-Jugoslavia"	
-		},
-		{
-			"name": "Bulgaria",
-			"x": "Right:Italia,Percent:65",
-			"y": "Down:Romania,Percent:-22",
-			"width": 59,
-			"height": 26,
-			"rotation": 0,
-			"sprite": "Bulgaria"	
-		},
-			{
-			"name": "Kreikka",
-			"x": "Right:Italia,Percent:13",
-			"y": "Down:Romania,Percent:28",
-			"width": 78,
-			"height": 54,
-			"rotation": 0,
-			"sprite": "Kreikka"	
-		},
-			{
-			"name": "Iso-Britannia",
-			"x": "Same:Ranska,Percent:-50",
-			"y": "Up:Ranska,Percent:10",
-			"width": 84,
-			"height": 119,
-			"rotation": 0,
-			"sprite": "Iso-Britannia"	
-		},
-			{
-			"name": "Islanti",
-			"x": "Same:Iso-Britannia,Percent:-50",
-			"y": "Up:Iso-Britannia,Percent:-60",
-			"width": 92,
-			"height": 48,
-			"rotation": 0,
-			"sprite": "Islanti"	
-		},
-		{
-			"name": "Turkki",
-			"x": "Right:Kreikka,Percent:0",
-			"y": "Same:Kreikka,Percent:10",
-			"width": 186,
-			"height": 53,
-			"rotation": 0,
-			"sprite": "Turkki"	
-		}/*,
-		
-		{
-			"name": "Albania",
-			"x": "Right:Italia,Percent:33",
-			"y": "Down:Romania,Percent:33",
-			"width": 78,
-			"height": 54,
-			"rotation": 0,
-			"sprite": "Albania"	
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*,	
+		{
+			"name": "Karttapohja",
+			"x": " ,Border:Right",
+			"y": "600",
+			"width": 1540,
+			"height": 1013,
+			"rotation": 0,
+			"sprite": "Karttapohja"
+		}		
 */
 		
 
