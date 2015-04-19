4a5288bbf3c9f99d73af150764001a2907571e00
Tero Paavolainen <tero.s.t.paavolainen@student.jyu.fi>
2015-04-19 17:06:18 +0300

    lowered bitrate of music (for i-things)

--- src/Booter.js
+++ src/Booter.js
@@ -12,7 +12,7 @@ Booter.prototype.init = function(){
 
 Booter.prototype.preload = function(){
 	this.phaserGame.load.image('logo', PICTURE_PATH+'PelinLogo.png');
-	this.phaserGame.load.audio('music', SOUND_PATH+'War.ogg');
+	this.phaserGame.load.audio('music', SOUND_PATH+'WarLow.ogg');
 }
 
 Booter.prototype.create = function(){
